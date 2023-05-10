32aa26d13dbf6e0530e466dffff2828e23b461b6
lvntky <klevent1903@gmail.com>
2023-05-10 11:37:12 +0300

    bytecode reader

--- src/class.c
+++ src/class.c
@@ -1,19 +1,22 @@
 #include "../include/class.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *get_bytecode(const char *filename)
 {
   FILE *fileptr = fopen(filename, "rb");
   char *buffer = (char *)malloc(2 * sizeof(char));
-
+  buffer[0] = '\0';
+  unsigned char byte;
+  while(fread(&byte, sizeof(byte), 1, fileptr) == 1) {
+    char *temp = (char *) malloc(3 * sizeof(char));
+    sprintf(temp, "%02x", byte);
+    buffer = (char *) realloc(buffer, strlen(buffer) + strlen(temp) + 1);
+    strcat(buffer, temp);
+    free(temp);
+  }
 
 
   return buffer;
-}
-
-int main(int argc, char** argv)
-{
-  get_bytecode(argv[1]);
-  return 0;
 }
