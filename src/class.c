763007c854228ab3906729c157cf12dd2f5fc178
lvntky <klevent1903@gmail.com>
2023-05-12 22:46:57 +0300

    driver code updated

--- src/class.c
+++ src/class.c
@@ -11,12 +11,19 @@ classfile read_classfile(const char* filename)
   fread(&classfile.magic, sizeof(classfile.magic), 1, fileptr);
   classfile.magic = be32toh(classfile.magic);
   printf("MAGIC: %X\n", classfile.magic);
+
   fread(&classfile.minor_version, sizeof(classfile.minor_version), 1, fileptr);
   classfile.minor_version = be16toh(classfile.minor_version);
   printf("MINOR: %x\n", classfile.minor_version);
+
   fread(&classfile.major_version, sizeof(classfile.major_version), 1, fileptr);
   classfile.major_version = be16toh(classfile.major_version);
-  printf("MAJOR: %x", classfile.major_version);
+  printf("MAJOR: %x\n", classfile.major_version);
+
+  fread(&classfile.constant_pool_count, sizeof(classfile.constant_pool_count), 1, fileptr);
+  classfile.constant_pool_count = be16toh(classfile.constant_pool_count);
+  printf("CONSTANT POOL COUNT: %d\n", classfile.constant_pool_count);
+
   fclose(fileptr);
   return classfile;
 }
